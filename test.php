9d0905f09da338e5ba56dae6c1542d38c12e8b2b
Aravindail <vasilevich-a-s@mail.ru>
2019-11-01 10:52:31 +0300

    Start dev

--- test.php
+++ test.php
@@ -2,6 +2,7 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("test");
 
-?><?
+?><pre><?=LANG_CHARSET?></pre>
+<?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
 ?>
