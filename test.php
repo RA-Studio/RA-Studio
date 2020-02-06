e201c0d05c31b6f2178fe32d7722d90928775d47
Aravindail <vasilevich-a-s@mail.ru>
2020-02-06 14:04:04 +0300

    Блог конец

--- test.php
+++ test.php
@@ -1,54 +1,105 @@
 <?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("test");
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
 
-?><pre></pre><?$APPLICATION->IncludeComponent("slam:easyform", "blogCallBack", Array(
-	"COMPONENT_TEMPLATE" => "uniform",
-		"FORM_ID" => "FORM2",	// ID формы
-		"FORM_NAME" => "",	// Название формы
-		"WIDTH_FORM" => "500px",	// Ширина формы
-		"DISPLAY_FIELDS" => array(	// Поля (символьный код)
-			0 => "MESSAGE",
-			1 => "",
-		),
-		"REQUIRED_FIELDS" => array(	// Обязательные поля
-			0 => "MESSAGE",
-		),
-		"FIELDS_ORDER" => "MESSAGE",	// Расположение полей формы
-		"CLEAR_FORM" => "N",	// Кнопка очистить форму
-		"FORM_AUTOCOMPLETE" => "Y",	// Автокомплит значений полей формы
-		"HIDE_FIELD_NAME" => "Y",	// Скрывать названия полей  формы
-		"HIDE_ASTERISK" => "Y",	// Убрать двоеточие и звездочки
-		"FORM_SUBMIT_VALUE" => "Отправить",	// Название кнопки
-		"SEND_AJAX" => "Y",	// Отправлять форму при помощи AJAX?
-		"SHOW_MODAL" => "N",	// Показывать результат в модальном окне
-		"_CALLBACKS" => "success_FORM2",	// Название функции при успешной отправки ( "_callbacks" )
-		"OK_TEXT" => "Ваше сообщение отправлено. Мы свяжемся с вами в течение 2х часов",	// Сообщение об успешной отправке
-		"ERROR_TEXT" => "Произошла ошибка. Сообщение не отправлено.",	// Сообщение об ошибке
-		"ENABLE_SEND_MAIL" => "Y",	// Включить отправку писем
-		"CREATE_SEND_MAIL" => "",	// Создание нового почтового шаблона
-		"EVENT_MESSAGE_ID" => array(	// Шаблон письма
-			0 => "48",
-		),
-		"EMAIL_TO" => "",	// E-mail, на который будет отправлено письмо (по умолчанию используется из настроек модуля)
-		"EMAIL_BCC" => "",	// Скрытая копия
-		"MAIL_SUBJECT_ADMIN" => "#SITE_NAME#: Сообщение из формы обратной связи",	// Тема сообщения для администратора
-		"EMAIL_FILE" => "N",	// Прикладывать файлы к письму
-		"USE_IBLOCK_WRITE" => "N",	// Записывать результаты в ИБ
-		"CATEGORY_MESSAGE_TITLE" => "Сообщение",	// Название
-		"CATEGORY_MESSAGE_TYPE" => "textarea",	// Тип поля
-		"CATEGORY_MESSAGE_PLACEHOLDER" => "Мы ценим ваше мнение",	// Placeholder
-		"CATEGORY_MESSAGE_CLASS" => "general-itemInput",	// Класс (general-itemInput_33 general-itemInput_half general-itemInput_25)
-		"CATEGORY_MESSAGE_VALUE" => "",	// Значение
-		"USE_CAPTCHA" => "N",	// Использовать капчу reCAPTCHA
-		"USE_MODULE_VARNING" => "N",	// Использовать сообщение из настроек модуля
-		"FORM_SUBMIT_VARNING" => "",	// Сообщение, выводимое перед кнопкой
-		"USE_FORMVALIDATION_JS" => "N",	// Проверять поля через JS Bootstrap Validators
-		"USE_JQUERY" => "N",	// Подключить jQuery-1.12.4
-		"USE_BOOTSRAP_CSS" => "N",	// Подключить стандартные стили Bootstrap 3
-		"USE_BOOTSRAP_JS" => "N",	// Подключить стандартный JS Bootstrap 3
-	),
-	false
-);?><?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
+//из 1с
+//пример данных из 1с
+$json = /*!empty($_POST)?$_POST['param']:*/'[{"0":{"id":"71996","ПоследнееИзменение":"28.12.2019 23:20:58","СтатусСчета":"Формируется"}},"1":{"id":"71995","ПоследнееИзменение":"23.12.2019 23:20:58","СтатусСчета":"Формируется"}]';//'{"0":{"id":"71995","ПоследнееИзменение":"10.11.2019 18:56:04","СтатусСчета":"Новый"}}';
+$obj = json_decode($json,true);
+
+//в 1с
+
+$orders = [];
+$min5 = time() - 60*5*5000000;
+$arFilter = Array(">=DATE_UPDATE" => date($DB->DateFormatToPHP(CSite::GetDateFormat("SHORT")), $min5));
+$dbSales = CSaleOrder::GetList(Array("ID" => "ASC"),array()/*$arFilter*/, false, false,array("ID","STATUS_ID","PAY_SYSTEM_ID","DATE_UPDATE","DISCOUNT_VALUE"));
+
+while ($ar_sales = $dbSales->Fetch()) {
+    switch ($ar_sales['STATUS_ID']){
+        case 'ND':
+        case 'N':
+            $status = 'Новый';
+            break;
+        case 'VM':
+            $status = 'В магазине';
+            break;
+        case 'F':
+            $status = 'Выполнен';
+            break;
+        case 'C':
+            $status = 'Отменен';
+            break;
+        default:
+            $status='';
+            break;
+    }
+    switch ($ar_sales['PAY_SYSTEM_ID']){
+        case 1:
+            $payment = 'Оплата наличными';
+            break;
+        case 13:
+            $payment = 'Оплачено yandex';
+            break;
+        case 16:
+            $payment = 'Оплачен БН';
+            break;
+        case 14:
+        case 18:
+            $payment = 'Оплата сбербанк';
+            break;
+        default:
+            $payment='';
+            break;
+    }
+    array_push($orders, ["id"=>$ar_sales['ID'],"ПоследнееИзменение"=>$ar_sales['DATE_UPDATE'],"СтатусСчета"=>$status, "Оплата"=>$payment, "Скидка"=>$ar_sales['DISCOUNT_VALUE'] ]);
+    foreach($obj as $key=>$order){
+        if ($order['id']==$ar_sales["ID"]){
+            unset($obj[$key]);
+        }
+    }
+}
+print_r($obj);
+/**///изменить заказы
+foreach ($obj[0] as $order) {
+
+    $arOrder = CSaleOrder::GetByID($order["id"]);
+    if ($arOrder) {
+        echo "<pre>";
+        print_r($order['id']);
+        print_r($arOrder);
+        echo "</pre>";
+        if(strtotime($order['ПоследнееИзменение']) >strtotime($arOrder['DATE_UPDATE'])){
+            switch ($order['СтатусСчета']){
+                case 'Новый':
+                    $status = 'ND';
+                    break;
+                case 'Дособрать':
+                case 'Формируется':
+                    $status = 'FO';
+                    break;
+                case 'В магазине':
+                    $status = 'VM';
+                    break;
+                case 'Выполнен':
+                    $status = 'F';
+                    break;
+                case 'Отмена (нет товара)':
+                case 'Отменен':
+                    $status = 'C';
+                    break;
+                default:
+                    $status='';
+                    break;
+            }
+            print_r($order);
+
+            if($status!=''){
+                $arFields = array("STATUS_ID" => $status);
+                CSaleOrder::Update($order["id"], $arFields);
+            }
+        }
+    }
+}
+echo '<pre>';
+//print_r($orders/*json_encode($orders,JSON_UNESCAPED_UNICODE)*/);
+echo '</pre>';
 ?>
